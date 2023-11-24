b1e8985efd6c0959aaf539005caa806394f6d9d0
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-24 14:52:38 +0100

    Add play alert functionality when mouseUp

--- src/ContinuousSlider.jsx
+++ src/ContinuousSlider.jsx
@@ -4,9 +4,17 @@ import Stack from "@mui/material/Stack";
 import Slider from "@mui/material/Slider";
 import VolumeDown from "@mui/icons-material/VolumeDown";
 import VolumeUp from "@mui/icons-material/VolumeUp";
+import beepAlert from "./audio/beep.mp3";
+
+const alert = new Audio(beepAlert);
 
 export default function ContinuousSlider() {
   const [value, setValue] = React.useState(30);
+
+  function handleSetVolume() {
+    alert.volume = value / 100;
+    alert.play();
+  }
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -16,7 +24,12 @@ export default function ContinuousSlider() {
     <Box sx={{ width: 200 }}>
       <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
         <VolumeDown />
-        <Slider aria-label="Volume" value={value} onChange={handleChange} />
+        <Slider
+          aria-label="Volume"
+          value={value}
+          onChange={handleChange}
+          onMouseUp={() => handleSetVolume()}
+        />
         <VolumeUp />
       </Stack>
     </Box>

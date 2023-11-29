db2e22cb625e1f5f2727f95f82af27336146be54
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-29 11:53:19 +0100

    Add useEffect

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -8,7 +8,16 @@ export default function Timer() {
   const [minTime, setMinTime] = useState(2);
   const [maxTime, setMaxTime] = useState(3);
   const alertSound = new Audio(beepAlert);
+
   let pauseTimer;
+
+  useEffect(() => {
+    pauseTimer = false;
+
+    if (isTimerOn) {
+      setTimer();
+    }
+
     async function setTimer() {
       const randomTime = getRandomTime(minTime, maxTime);
 
@@ -20,6 +29,12 @@ export default function Timer() {
 
       if (!pauseTimer) setTimer();
     }
+
+    return () => {
+      pauseTimer = true;
+    };
+  }, [isTimerOn]);
+
   function timerDelay(delay) {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -43,42 +58,43 @@ export default function Timer() {
     setIsTimerOn(!isTimerOn);
   }
 
+
   return (
     <main>
       <div className="small-container">
         <h1>Random Timer</h1>
         {isTimerOn ? null : (
           <>
-        <div className="minmax-container">
-          <div className="min-timer">
-            <label htmlFor="min-input">Min time:</label>
-            <input
-              id="min-input"
-              type="number"
-              min={0}
-              max={60}
-              placeholder="min"
+            <div className="minmax-container">
+              <div className="min-timer">
+                <label htmlFor="min-input">Min time:</label>
+                <input
+                  id="min-input"
+                  type="number"
+                  min={0}
+                  max={60}
+                  placeholder="min"
                   value={minTime}
                   onChange={(e) => setMinTime(Number(e.target.value))}
-            />
-          </div>
-          <div className="max-timer">
-            <label htmlFor="max-input">Max time:</label>
+                />
+              </div>
+              <div className="max-timer">
+                <label htmlFor="max-input">Max time:</label>
 
-            <input
-              type="number"
-              id="max-input"
-              min={1}
-              max={60}
-              placeholder="min"
+                <input
+                  type="number"
+                  id="max-input"
+                  min={1}
+                  max={60}
+                  placeholder="min"
                   value={maxTime}
                   onChange={(e) => setMaxTime(Number(e.target.value))}
-            />
-          </div>
-        </div>
-        <div className="volume-container">
-          <InputSlider />
-        </div>
+                />
+              </div>
+            </div>
+            <div className="volume-container">
+              <InputSlider />
+            </div>
           </>
         )}
 
